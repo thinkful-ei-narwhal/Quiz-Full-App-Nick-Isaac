41002abd97511f0e6f0a74690b33bc20601c5b5a
nevelson <nevelson92@gmail.com>
2020-03-19 19:03:56 -0400

    Started Quiz app

--- app.js
+++ app.js
@@ -78,11 +78,136 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function generateStartPageHTML() {
+  return `<h2>Welcome to my space Quiz!</h2>
+  <h2>Let's see if you can answer some esoteric questions!</h2>
+
+  <button class="start-quiz">
+    Start Quiz
+  </button>`;
+}
+
+function generateQuestionPageFormHTML() {
+  const questionArrayNum = store.questionNumber;
+  const questionsObj = store.questions[questionArrayNum];
+  const answersArray = questionsObj.answers;
+
+  const arrayQuestions = answersArray.map((element, index) => {
+    return `<input type="radio" id="ans${index}" name="answer" />
+    <label for="asn${index}">${element}</label><br>`;
+  });
+
+  return `<section class="question">
+  <h3>${store.score} Correct, ${questionArrayNum - store.score} incorrect</h3>
+  <h3>Question ${questionArrayNum + 1}</h3>
+  <p>What is bootes void?</p>
+  <form action="#" id="question-form">
+  <div class="radio-buttons">
+  ${arrayQuestions.join('')}
+  </div>
+  <input type="submit" class="submit-button" name="submit-answer" value="Submit Answer">
+  </form>
+</section>`;
+}
+
+function generateCorrectPageHTML() {
+  //generate the SpaceQuizHeader
+  //Generate the correct message
+  //generate the question that was asked
+}
+
+function generateIncorrectPageHTML() {
+  //generate the SpaceQuizHeader
+  //generate the incorrect message
+  //generate the question that was asked
+  //find the correctAnswer from the store and generate that
+}
+
+function generateResultsPageHTML() {
+  //generate the SpaceQuizHeader
+  //read how many questions they got right and wrong from store
+  //update the numbers in thet questions correct/incorrect html
+  //calculate the percentage out of total and generate that HTML
+  //generate the retake button html
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
+function render() {
+  if (store.quizStarted === false) {
+    $('main').html(generateStartPageHTML());
+  }
+  else {
+    $('main').html(generateQuestionPageFormHTML());
+  }
+  //TODO: need logic to figure out rendering correct and incorrect pages
+
+  handleButtons();
+}
+
+function handleButtons() {
+  startQuizHandler();
+  submitAnswerHandler();
+  nextQuestionHandler();
+  retakeQuizHandler();
+}
+
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function startQuizHandler() {
+  $('.start-quiz').click(function (event) {
+    console.log('Starting quiz');
+    store.quizStarted = true;
+    render();
+  });
+}
+
+function submitAnswerHandler() {
+  $('#question-form').submit(function (event) {
+    event.preventDefault();
+    console.log('Answer submitted');
+
+    const radioValue = $('input[name=\'answer\']:checked + label').text();
+    const question = store.questions.find(element => element.answers.find(elementAnswer => elementAnswer === radioValue));
+    if (radioValue === '') {
+      console.log('User did not select an answer');
+      alert('You need to select an answer');
+    } else {
+      console.log(`Selected value is: ${radioValue}`);
+      if (radioValue === store.questions[store.questionNumber].correctAnswer) {
+        store.score++;
+        console.log('TESTING');
+        //TODO: if answer is correct, render correct page
+      }
+      else {
+        console.log('TESTING2');
+        //TODO: if answer is incorrect, render the incorrect page
+      }
+      render();
+    }
+  });
+}
+
+//deals with the submit button for picking an answer to the question
+//Accesses the store and questionNumber++
+//calculates if answer matches correct answer, if so, access store.score and adds ++
+//Runs render()
+
+
+function nextQuestionHandler() {
+  //deals with the next question buttons on the correct and incorrect pages
+  // not sure yet...
+  //Runs render()
+}
+
+function retakeQuizHandler() {
+  //resets the store to it's default state
+  //Runs render()
+}
+
+// -----------Calling render to start the application --------
+render();
