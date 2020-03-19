30cfe84486a2dacfce4cdcf28aa0e4d358edf44a
nevelson <nevelson92@gmail.com>
2020-03-19 16:49:22 -0400

    Added our store

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
 'use strict';
-/**
- * Example store structure
- */
+
 const store = {
-  // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What is Bootes void?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'A ‘dead zone’ in space where there are no stars for hundreds of millions of miles in every direction',
+        'The outer reaches of space that stretch past the cosmic backdrop of the universe',
+        'The event horizon between galactic-level black holes and the nearest star',
+        'The space between galaxies'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'A ‘dead zone’ in space where there are no stars for hundreds of millions of miles in every direction',
     },
     {
-      question: 'What is the current year?',
+      question: 'What is a Pulsar?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'A comet that is large enough to destroy our planet if it were to hit.',
+        'A subatomic particle that can only be captured under ground in vats of heavy-water',
+        'A celestial body that spins extremely fast and emits energy from its poles',
+        'A cosmic body that travels as fast as the speed of light'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'A celestial body that spins extremely fast and emits energy from its poles'
+    },
+    {
+      question: 'What is a supernova?',
+      answers: [
+        'A planet that has collapsed under its own weight and turned into a black hole',
+        'A star that has exploded',
+        'Space that has folded in onto itself',
+        'A new star being born'
+      ],
+      correctAnswer: 'A star that has exploded'
+    },
+    {
+      question: 'What does it mean for mercury to be in retrograde?',
+      answers: [
+        'The planet is moving closer to our sun',
+        'The planet appears to have reversed its orbit',
+        'The planet is preparing to emit mass from its poles',
+        'A cosmological warning that you will have bad luck this month'
+      ],
+      correctAnswer: 'The planet appears to have reversed its orbit'
+    },
+    {
+      question: ' What is a geo heliocentric universe?',
+      answers: [
+        'A model for our universe where the sun and earth are at its center',
+        'A model for our universe where stars and planets orbit each other instead of planets orbiting stars',
+        'A model for our universe where our solar system is orbited by other solar systems',
+        'A model for a solar system where planets are slowly sucked in by the sun\'s gravity'
+      ],
+      correctAnswer: 'A model for our universe where the sun and earth are at its center'
     }
   ],
   quizStarted: false,
   questionNumber: 0,
   score: 0
 };
+
+
 
 /**
  *
